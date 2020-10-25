2acfbb8d4cd0093cd1411770309e1fbb571fb8c9
Jefferson Brioso <jabbsontouch@gmail.com>
2020-10-24 21:51:01 -0400

    gitignore y package_json

--- docto_user/src/app/doctors/doctors.page.ts
+++ docto_user/src/app/doctors/doctors.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { MedicoService } from "../../services/medico.service";
+import { MedicoService } from "../../services/medico/medico.service";
 
 @Component({
   selector: "app-doctors",
